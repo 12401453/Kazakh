65d1ae166f071a3ca5de774b6ea25b892501a8f7
12401453 <12401453@protonmail.com>
2022-12-30 03:20:32 +0000

    ..

--- text_viewer.php
+++ text_viewer.php
@@ -37,9 +37,17 @@ $res = $conn->query($sql);
 
 <div id="select_button">
   <label for="textselect">Choose text:</label>
-  <select id="textselect" name="textselect" onchange="selectText()">
+  <select id="textselect" name="textselect" onchange="selectText()" style="width: 100px;">
   <option value="0"></option>
   <?php 
+
+  if(isset($_COOKIE["text_id"])) {
+    $prev_text_id = $_COOKIE["text_id"];
+  }
+  else {
+    $prev_text_id = 0;
+  }
+  
 
   $sql = "SELECT * FROM texts";
   $res = $conn->query($sql);
@@ -59,8 +67,15 @@ $res = $conn->query($sql);
 
   <?php include 'controls.php'; ?><br>
   
-<p1 id="p1"><br><br>
-
+<p1 id="p1">
+  <?php 
+  if($prev_text_id == 0) {
+    echo '<br><br>';
+  }
+  else {
+    include 'voidRetrieveText.php';
+  }
+  ?>
 </p1>
 
 </div>
@@ -75,6 +90,7 @@ $res = $conn->query($sql);
 </html>
 
 <script>
+  /*
   const textselector = document.getElementById('textselect');
 
 textselector.addEventListener('change', event => {
@@ -92,5 +108,60 @@ textselector.addEventListener('change', event => {
       
       dummyselect.remove();
 });
-textselector.style.width = "100px";
+textselector.style.width = "100px"; */
+
+let cookie_textselect = <?php echo $prev_text_id; ?>
+
+const textselector = document.getElementById('textselect');
+const textselect_resize = (event) => {
+  let dummyselect = document.createElement('select');
+  let dummyoption = document.createElement('option');
+
+  dummyoption.textContent = event.target.options[event.target.selectedIndex].text;  //event.target is the <select> element; .options is a list of its options
+  // dummyselect.style.cssText += "visibility: hidden; position: fixed; font-family: Calibri; font-size: 18px;";
+  dummyselect.id = 'hidden_select';
+  dummyselect.appendChild(dummyoption);
+  event.target.after(dummyselect);//.after('') inserts stuff directly after the entire element, i.e. after its </> closing tag
+  
+  const dummywidth = dummyselect.getBoundingClientRect().width;
+  event.target.style.width = `${dummywidth}px`;
+  
+  dummyselect.remove();
+};
+
+textselector.addEventListener('change', textselect_resize);
+textselector.addEventListener('cookie_selection', textselect_resize);
+
+window.onload = () => {    
+    if(cookie_textselect) {
+      let textselect = document.getElementById("textselect");
+      let text_options = textselect.options;
+      let textid_deleted = true;
+      for(let i = 0; i < text_options.length; i++) {
+        if(text_options[i].value == cookie_textselect){
+            textid_deleted = false;
+            text_options[i].selected = true;
+            textselect.dispatchEvent(new Event('cookie_selection'));
+            setLangId();
+            break;
+        }
+      }
+      if(textid_deleted == false) {
+        let pagenos = document.querySelectorAll('.pageno');
+        pagenos.forEach(pageno => {
+          if(Number(pageno.innerHTML) == 1) {
+            pageno.classList.add("current_pageno");
+          }
+        });
+
+        let tt_btns = document.querySelectorAll('.tooltip');
+
+        tt_btns.forEach(tt_btn => {
+          tt_btn.onclick = showAnnotate;
+        });
+      }
+      else console.log("cookie text has been deleted by another client");   
+    }
+    else console.log("no text_id cookie");
+};
 </script>
--- voidRetrieveText.php
+++ voidRetrieveText.php
@@ -1,32 +1,5 @@
 <?php
-
-$text_id = '';
-if(isset($_POST['textselect'])) {
-  $text_id = $_POST['textselect'];
-  setcookie("text_id", $text_id);
-}
-else {$text_id = 0;}
-if ($text_id == 0) {
-  echo "<br><br>";
-  exit(0); 
-}
-
-
-
-
-include 'db_details_web.php';
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-
-$sql = "SET NAMES UTF8";
-$res = $conn->query($sql);
-
-$sql = "SELECT dt_start, dt_end, text_title FROM texts WHERE text_id = $text_id";
+$sql = "SELECT dt_start, dt_end, text_title FROM texts WHERE text_id = $prev_text_id";
 $res = $conn->query($sql);
 $row = $res->fetch_assoc();
 
@@ -150,6 +123,4 @@ if ($result->num_rows > 0) {
     } else {
       echo "";
     }
-    $conn->close();
-    
     ?>
