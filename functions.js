2c35731fe2bc61a9767d4928803f57928abe5fb8
12401453 <12401453@protonmail.com>
2023-02-12 11:39:36 +0000

    ..

--- functions.js
+++ functions.js
@@ -630,9 +630,9 @@ const switchMeaning = function (event) {
   if (lemma_id != 0 && meanings[lemma_meaning_no] === undefined) {
     switchMeaningAJAX();
   }
- /* else if (lemma_id != 0) {  */
+
   document.getElementById("lemma_textarea").value = meanings[lemma_meaning_no] == undefined ? "" : meanings[lemma_meaning_no];
-  // }
+  document.getElementById("lemma_textarea").focus();
   
 };
 //this is just placeholder
